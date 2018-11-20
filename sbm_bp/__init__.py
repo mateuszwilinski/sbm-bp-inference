a50a6c2095b166c1c68607557207e23ef9f12c27
mateuszwilinski <mateuszwilinski@sns.it>
2018-11-20 14:36:58 +0100

    test refactoring and fixes

--- sbm_bp/__init__.py
+++ sbm_bp/__init__.py
@@ -1,3 +1,2 @@
-# __init__.py
 from .messages import Messages, DirectedMessages
 from .functions import get_real_overlap, affinity, directed_affinity, random_marginals
--- test/test_messages.py
+++ test/test_messages.py
@@ -1,12 +1,12 @@
 import unittest as ut
 import numpy as np
 import igraph as ig
-from sbm_bp import Messages, get_real_overlap, affinity
+from sbm_bp import Messages, affinity
 
 
 class TestMessages(ut.TestCase):
     def test_overlap(self):
-        N = 500  # number of nodes
+        N = 1000  # number of nodes
         q = 2  # number of blocks
         c = 3  # average degree
         eps = 0.7  # order parameter
@@ -33,4 +33,26 @@ class TestMessages(ut.TestCase):
             conv = msg.update_messages()
             t += 1
 
-        self.assertAlmostEqual(msg.get_overlap(), 0.9, places=1)
+        self.assertAlmostEqual(msg.get_overlap(), 0.0, places=1)
+
+        eps = 0.1  # order parameter
+
+        p = affinity(q, c, n, eps, N)
+        G = ig.Graph.SBM(N, p.tolist(), (N * n).tolist(), directed=False)
+
+        groups = np.zeros(N, dtype='int')
+        for i in range(q - 1):
+            groups[int(N * np.sum(n[:(i + 1)])):] = i + 1
+        G.vs['group'] = groups
+
+        msg = Messages(q, n, N * np.array(p), G)
+        msg.update_marginals()
+        msg.update_field()
+
+        conv = crit + 10.0
+        t = 0
+        while conv > crit and t < t_max:
+            conv = msg.update_messages()
+            t += 1
+
+        self.assertAlmostEqual(msg.get_overlap(), 0.8, places=1)
