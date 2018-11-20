e8e3e2ce5fad344d0db8256b850718257d4c06b3
mateuszwilinski <mateuszwilinski@sns.it>
2018-11-20 18:12:02 +0100

    added unit tests for directed case

--- test/test_messages.py
+++ test/test_messages.py
@@ -1,7 +1,7 @@
 import unittest as ut
 import numpy as np
 import igraph as ig
-from sbm_bp import Messages, affinity
+from sbm_bp import Messages, DirectedMessages, affinity, directed_affinity
 
 
 class TestMessages(ut.TestCase):
@@ -9,10 +9,11 @@ class TestMessages(ut.TestCase):
         N = 1000  # number of nodes
         q = 2  # number of blocks
         c = 3  # average degree
-        eps = 0.7  # order parameter
 
         crit = 0.01
         t_max = 50
+
+        eps = 0.7  # order parameter
 
         n = np.ones(q) / q
         p = affinity(q, c, n, eps, N)
@@ -46,6 +47,52 @@ class TestMessages(ut.TestCase):
         G.vs['group'] = groups
 
         msg = Messages(q, n, N * np.array(p), G)
+        msg.update_marginals()
+        msg.update_field()
+
+        conv = crit + 10.0
+        t = 0
+        while conv > crit and t < t_max:
+            conv = msg.update_messages()
+            t += 1
+
+        self.assertAlmostEqual(msg.get_overlap(), 0.8, places=1)
+
+        eps = 0.7  # order parameter
+        gamma = 0.8  # symmetry order parameter
+
+        p = directed_affinity(q, c, n, eps, gamma, N)
+        G = ig.Graph.SBM(N, p.tolist(), (N * n).tolist(), directed=True)
+
+        groups = np.zeros(N, dtype='int')
+        for i in range(q - 1):
+            groups[int(N * np.sum(n[:(i + 1)])):] = i + 1
+        G.vs['group'] = groups
+
+        msg = DirectedMessages(q, n, N * np.array(p), G)
+        msg.update_marginals()
+        msg.update_field()
+
+        conv = crit + 10.0
+        t = 0
+        while conv > crit and t < t_max:
+            conv = msg.update_messages()
+            t += 1
+
+        self.assertAlmostEqual(msg.get_overlap(), 0.0, places=1)
+
+        eps = 0.1  # order parameter
+        gamma = 0.8  # symmetry order parameter
+
+        p = directed_affinity(q, c, n, eps, gamma, N)
+        G = ig.Graph.SBM(N, p.tolist(), (N * n).tolist(), directed=True)
+
+        groups = np.zeros(N, dtype='int')
+        for i in range(q - 1):
+            groups[int(N * np.sum(n[:(i + 1)])):] = i + 1
+        G.vs['group'] = groups
+
+        msg = DirectedMessages(q, n, N * np.array(p), G)
         msg.update_marginals()
         msg.update_field()
 
